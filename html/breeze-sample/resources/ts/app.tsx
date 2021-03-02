59e6d63fae0337f3bd3c4cc63d2720cb0fd9a669
developer-kumamoto <developer.kumamoto@gmail.com>
2021-03-02 21:08:38 +0900

    react dev branch
    main with react modules.

---
+++ html/breeze-sample/resources/ts/app.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+
+const App: React.FC = () => {
+    return (
+        <div>
+　　　　　   Hello World
+        </div>
+    )
+}
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'));
+}
